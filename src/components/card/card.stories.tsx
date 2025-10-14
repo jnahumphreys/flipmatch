7326b75f63fcb3edf3d5a7f454b1c3772d9339e4
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2025-10-14 09:15:43 +0000

    chore: remove boilerplate comment from Card story

--- src/components/card/card.stories.tsx
+++ src/components/card/card.stories.tsx
@@ -15,7 +15,6 @@ const meta = {
       control: false,
     },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
     onClick: fn(),
     onLoad: fn(),
