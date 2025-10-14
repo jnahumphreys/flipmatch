8da7ba2513082ec00640bcea856fd661e89ee818
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2025-10-14 13:31:06 +0000

    feat: reduce grid gap and make flexible

--- src/components/stage/stage.tsx
+++ src/components/stage/stage.tsx
@@ -2,7 +2,7 @@ import { Card } from "../card";
 
 function Stage() {
   return (
-    <div className="grid max-w-166 grid-cols-5 grid-rows-4 gap-2 sm:gap-4">
+    <div className="grid aspect-square w-full max-w-158 grid-cols-5 grid-rows-5 gap-[1.5%]">
       {[...Array(25)].map((_, i) => (
         <Card
           key={i}
